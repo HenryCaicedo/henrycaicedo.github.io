2efecc24f756843916a1830f571696a2c55b48ef
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2024-01-15 01:02:57 -0500

    NavBar moved to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import './App.css'
-import NavBar from './components/NavBar'
+import React from 'react';
+import NavBar from './components/NavBar';
 
 function App() {
-
   return (
     <>
       <nav>
-        <NavBar/>
+        <NavBar />
       </nav>
+
+      <main>
+        {/* Your main content goes here */}
+        <p>This is the body content.</p>
+      </main>
+
+      <footer>
+        {/* Your footer content goes here */}
+        <p>This is the footer.</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,25 +5,31 @@ interface ItemProps {
 }
 
 function Item({ word }: ItemProps) {
-  return  <li>
-            <button className='bg-blue-200 rounded-none'>  
+  return  <li className='flex items-center mt-2 hover:mt-0'>
+            <button className='px-3 py-2 mb-2 bg-red-400 hover:bg-red-200 hover:border-b-2 hover:m-0'>  
             {word || 'missing'}
-
             </button>
           </li>;
 }
 
 export default function NavBar() {
   return (
-    <nav>
-      <ul className='flex space-x-3'>
-        <Item word="Home" />
-        <Item word="About" />
-        <Item word="Skills" />
-        <Item word="Project" />
-        <Item word="Contact" />
-      </ul>
-    </nav>
+    <>
+    <div className='flex justify-between items-center bg-blue-200 px-32 py-3'>
+      <div className='bg-purple-600 w-10 h-10 space-between'>
+        
+      </div>
+        <ul className='flex space-x-3 items-center'>
+          <Item word="Home" />
+          <Item word="About" />
+          <Item word="Skills" />
+          <Item word="Project" />
+          <Item word="Contact" />
+        </ul>
+
+
+    </div>
+    </>
   );
 }
 

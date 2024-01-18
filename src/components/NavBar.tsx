bf5a98fd1691a61813718039e4d6d1787dc818af
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2024-01-18 01:01:14 -0500

    scroll down arrow created

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,32 +5,45 @@ interface ItemProps {
 }
 
 function Item({ word }: ItemProps) {
-  return  <li>
-            <button className='py-2 px-3 border-b-2 border-transparent rounded-2xl hover:bg-opacity-60
-            hover:border-red-500 transition duration-200 hover:bg-red-500
-            text-lg font-semibold text-white'>  
-            {word || 'missing'}
-            </button>
-          </li>;
+  return <li>
+    <button className='py-2 px-3 border-b-[3px] border-transparent rounded-2xl hover:bg-opacity-60
+            hover:border-blue-500 transition-all duration-150 hover:bg-blue-500
+              active:border-b-0 active:mt-[3px]
+            text-lg font-semibold text-white'>
+      {word || 'missing'}
+    </button>
+  </li>;
+}
+
+function ContactButton() {
+  return <li>
+    <button className='py-2 px-3 border-b-[3px] rounded-2xl bg-opacity-60
+            border-red-500 transition-all duration-150 bg-red-500
+            hover:bg-opacity-70
+            active:border-b-0 active:mt-[3px]
+            text-lg font-semibold text-white'>
+       Contact
+    </button>
+  </li>;
 }
 
 export default function NavBar() {
   return (
     <>
-    <div className='flex justify-between items-center bg-[#1f1f1f] px-48 py-3'>
-      <div className='bg-purple-600 w-14 h-14 space-between rounded-lg'>
-        
-      </div>
-        <ul className='flex space-x-2 items-center'>
+      <div className='flex justify-between items-center bg-[#1f1f1f] px-48 py-3'>
+        <div className='bg-purple-600 w-14 h-14 space-between rounded-lg'>
+
+        </div>
+        <ul className='flex space-x-8 items-center'>
           <Item word="Home" />
           <Item word="About" />
           <Item word="Skills" />
           <Item word="Project" />
-          <Item word="Contact" />
+          <ContactButton/>
         </ul>
 
 
-    </div>
+      </div>
     </>
   );
 }
--- src/components/Sections/Home.tsx
+++ src/components/Sections/Home.tsx
@@ -1,27 +1,51 @@
 import React from 'react';
 
+
+
+function ScrollArrow() {
+  return (
+
+    <div className="w-64 h-64 overflow-hidden inline-block bg-green-800 relative">
+      <div className="h-12 w-12 border-8 border-red-500 -rotate-45 transform origin-top-left"></div>
+      <div className="h-2 w-10 bg-white absolute inset-1/2"
+      style={{
+        left: '50%',
+        transform: 'translateX(0rem)'
+        }}></div>
+      <div className="h-2 w-10 bg-white transform absolute bottom-2 left-0 rotate-20"></div>
+    </div>
+
+  )
+}
+
+
+
 export default function Home() {
   return (
-    <div className='h-screen flex items-center justify-center space-x-28'>
-      <div className='w-1/2 flex justify-end'>
-        <div className='bg-slate-600 w-60 h-60 rounded-full text-center'>
-          PHOTO
+    <div className='h-screen flex flex-col'>
+
+      <div className='items-center justify-center space-x-28 w-full flex flex-grow'>
+
+        <div className='w-1/2 flex justify-end'>
+          <div className='bg-slate-600 w-60 h-60 rounded-full text-center'>
+            PHOTO
+          </div>
         </div>
-        {//<img
-         // src='your-photo.jpg'
-         // alt='Your Photo'
-         // className='w-1/2 max-w-xs rounded-full'
-        //>
-        }
+
+        <div className='w-1/2 flex-col items-start'>
+          <h1 className='text-6xl font-bold mb-2'>
+            Hi,
+          </h1>
+          <h1 className='text-6xl font-bold mb-2'>
+            I'm Henry
+          </h1>
+          <h2 className='text-4xl font-semibold'>Web Developer.</h2>
+        </div>
+
       </div>
-      <div className='w-1/2 flex-col items-start'>
-        <h1 className='text-6xl font-bold mb-2'>
-          Hi,
-        </h1>
-        <h1 className='text-6xl font-bold mb-2'>
-          I'm Henry
-        </h1>
-        <h2 className='text-4xl font-semibold'>Web Developer.</h2>
+
+      <div className='w-full h-40 flex bg-slate-900 justify-center items-center'>
+        <ScrollArrow />
       </div>
     </div>
   );

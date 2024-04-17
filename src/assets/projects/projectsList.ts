0865476461f8d664f055c5b318ac2665b1f518ea
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2024-04-17 18:45:46 -0500

    image paths fixed

--- src/assets/projects/projectsList.ts
+++ src/assets/projects/projectsList.ts
@@ -3,28 +3,28 @@ const projectsList = [
         name: 'CodeRacers',
         description: 'Este es un breve resumen del proyecto tres.',
         tags: ['React', 'HTML5', 'UI/UX'],
-        img: '/src/assets/projects/images/cr.png', 
+        img: 'src/assets/projects/images/cr.png', 
         url: 'https://github.com/HenryCaicedo/final-project'
     },
     {
         name: 'Rackity',
         description: 'Este es un breve resumen del proyecto cuatro.',
         tags: ['Java', 'Spring Boot', 'Backend'],
-        img: '/src/assets/projects/images/rackity.png', 
+        img: 'src/assets/projects/images/rackity.png', 
         url: 'https://github.com/HenryCaicedo/rackity',
     },
     {
         name: 'Bungee Jumping Simulator',
         description: 'Este es un breve resumen del proyecto dos.',
         tags: ['Python', 'AI', 'UI/UX'],
-        img: '/src/assets/projects/images/bungee.png', 
+        img: 'src/assets/projects/images/bungee.png', 
         url: 'https://github.com/HenryCaicedo/BungeeJumpingSimulator',
     },
     {
         name: 'Women of Science',
         description: 'Este es un breve resumen del proyecto uno.',
         tags: ['JavaScript', 'React', 'Frontend'],
-        img: '/src/assets/projects/images/wos.png', 
+        img: 'src/assets/projects/images/wos.png', 
         url: 'https://github.com/leovergaramarq/WomenOfScience',
     },
     {

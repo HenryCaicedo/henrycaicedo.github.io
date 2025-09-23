7300ba791f99236de49ab21a49eb03727ff3538c
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2025-09-23 16:47:06 -0500

    feat: fruitos project added

--- src/assets/projects/projectsList.ts
+++ src/assets/projects/projectsList.ts
@@ -2,36 +2,37 @@ import rackity from './images/rackity.png';
 import cr from './images/cr.png';
 import wos from './images/wos.png';
 import bungee from './images/bungee.png';
-import covidSimulator from  './images/covidSimulator.png';
+import covidSimulator from './images/covidSimulator.png';
 import clientTrack from './images/clientTrack.png';
+import fruitOS from './images/fruitOS.png';
 
 const projectsList = [
     {
         name: 'CodeRacers',
         description: 'Web app designed to teach elementary school students programming by allowing them to code the path of a virtual car.',
         tags: ['React', 'Phaser', 'Web'],
-        img: cr, 
+        img: cr,
         url: 'https://github.com/HenryCaicedo/final-project'
     },
     {
         name: 'Rackity',
         description: "Inventory management app. Control and classification of the availability of items in the user's closet",
         tags: ['Flutter', 'UI/UX', 'Mobile'],
-        img: rackity, 
+        img: rackity,
         url: 'https://github.com/HenryCaicedo/rackity',
     },
     {
         name: 'Bungee Jumping Simulator',
         description: 'Physics-based simulator of the bungee jumping activity.',
         tags: ['Java', 'Simulator', 'Desktop'],
-        img: bungee, 
+        img: bungee,
         url: 'https://github.com/HenryCaicedo/BungeeJumpingSimulator',
     },
     {
         name: 'Women of Science',
-        description : "Educational game featuring Mary Jackson, Katherine Johnson, and Dorothy Vaughan as \"computers\" in early US space exploration.",
+        description: "Educational game featuring Mary Jackson, Katherine Johnson, and Dorothy Vaughan as \"computers\" in early US space exploration.",
         tags: ['Java', 'Game', 'Desktop'],
-        img: wos, 
+        img: wos,
         url: 'https://github.com/leovergaramarq/WomenOfScience',
     },
     {
@@ -42,13 +43,20 @@ const projectsList = [
         url: 'https://github.com/HenryCaicedo/Lab2_COVID_SIMULATOR',
     },
     {
+        name: 'FruitOS',
+        description: 'Retro-inspired clone of iOS 6 with skeuomorphic UI, glossy toggles, and shiny app icons.',
+        tags: ['React', 'UI/Design', 'Web'],
+        img: fruitOS,
+        url: 'https://github.com/HenryCaicedo/fruit-os-simulator',
+    },
+
+    {
         name: 'Client Track CRM',
         description: 'Customer relationship management tool for tracking client interactions and data.',
         tags: ['Next.js', 'Tailwind', 'Web'],
         img: clientTrack,
         url: 'https://github.com/HenryCaicedo/client-track-crm',
-    },
-
+    }
 ];
 
 export default projectsList;

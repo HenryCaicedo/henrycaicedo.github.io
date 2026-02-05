165e628295180a200ee67e8101f7102c4433adfe
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2026-02-05 09:25:19 -0500

    feat: shopify projects

--- src/assets/projects/projectsList.ts
+++ src/assets/projects/projectsList.ts
@@ -5,58 +5,73 @@ import bungee from './images/bungee.png';
 import covidSimulator from './images/covidSimulator.png';
 import clientTrack from './images/clientTrack.png';
 import fruitOS from './images/fruitOS.png';
+import caribeBrew from './images/caribeBrew.jpg';
+import liquidEmails from './images/liquidEmails.jpg';
 
-const projectsList = [
-    {
-        name: 'CodeRacers',
-        description: 'Web app designed to teach elementary school students programming by allowing them to code the path of a virtual car.',
-        tags: ['React', 'Phaser', 'Web'],
-        img: cr,
-        url: 'https://github.com/HenryCaicedo/final-project'
-    },
-    {
-        name: 'Rackity',
-        description: "Inventory management app. Control and classification of the availability of items in the user's closet",
-        tags: ['Flutter', 'UI/UX', 'Mobile'],
-        img: rackity,
-        url: 'https://github.com/HenryCaicedo/rackity',
-    },
-    {
-        name: 'Bungee Jumping Simulator',
-        description: 'Physics-based simulator of the bungee jumping activity.',
-        tags: ['Java', 'Simulator', 'Desktop'],
-        img: bungee,
-        url: 'https://github.com/HenryCaicedo/BungeeJumpingSimulator',
-    },
-    {
-        name: 'Women of Science',
-        description: "Educational game featuring Mary Jackson, Katherine Johnson, and Dorothy Vaughan as \"computers\" in early US space exploration.",
-        tags: ['Java', 'Game', 'Desktop'],
-        img: wos,
-        url: 'https://github.com/leovergaramarq/WomenOfScience',
-    },
-    {
-        name: 'COVID Simulator',
-        description: 'Graph-based COVID-19 spread simulator analyzing protective measures and contagion patterns.',
-        tags: ['Java', 'Simulator', 'Desktop'],
-        img: covidSimulator,
-        url: 'https://github.com/HenryCaicedo/Lab2_COVID_SIMULATOR',
-    },
-    {
-        name: 'FruitOS',
-        description: 'Retro-inspired clone of iOS 6 with skeuomorphic UI, glossy toggles, and shiny app icons.',
-        tags: ['React', 'UI/Design', 'Web'],
-        img: fruitOS,
-        url: 'https://fruit-os-simulator.vercel.app/',
-    },
+const projectsList = [{
+    name: 'Caribe Brew',
+    description: 'A fully branded Shopify store showcasing products and collections with a polished, user-friendly shopping experience..',
+    tags: ['Shopify', 'Web', 'Password: rtieyu'],
+    img: caribeBrew,
+    url: 'https://caribe-brew.myshopify.com',
+}, {
+    name: 'Liquid Emails',
+    description: "Demo of transactional email templates built with Liquid, used in Shopify stores.",
+    tags: ['Liquid', 'Shopify'],
+    img: liquidEmails,
+    url: 'https://github.com/HenryCaicedo/liquid-emails',
+},
+{
+    name: 'FruitOS',
+    description: 'Retro-inspired clone of iOS 6 with skeuomorphic UI, glossy toggles, and shiny app icons.',
+    tags: ['React', 'UI/Design', 'Web'],
+    img: fruitOS,
+    url: 'https://fruit-os-simulator.vercel.app/',
+},
+{
+    name: 'Client Track CRM',
+    description: 'Customer relationship management tool for tracking client interactions and data.',
+    tags: ['Next.js', 'Tailwind', 'Web'],
+    img: clientTrack,
+    url: 'https://github.com/HenryCaicedo/client-track-crm',
+},
 
-    {
-        name: 'Client Track CRM',
-        description: 'Customer relationship management tool for tracking client interactions and data.',
-        tags: ['Next.js', 'Tailwind', 'Web'],
-        img: clientTrack,
-        url: 'https://github.com/HenryCaicedo/client-track-crm',
-    }
+
+{
+    name: 'CodeRacers',
+    description: 'Web app designed to teach elementary school students programming by allowing them to code the path of a virtual car.',
+    tags: ['React', 'Phaser', 'Web'],
+    img: cr,
+    url: 'https://github.com/HenryCaicedo/final-project'
+},
+{
+    name: 'Rackity',
+    description: "Inventory management app. Control and classification of the availability of items in the user's closet",
+    tags: ['Flutter', 'UI/UX', 'Mobile'],
+    img: rackity,
+    url: 'https://github.com/HenryCaicedo/rackity',
+},
+{
+    name: 'Bungee Jumping Simulator',
+    description: 'Physics-based simulator of the bungee jumping activity.',
+    tags: ['Java', 'Simulator', 'Desktop'],
+    img: bungee,
+    url: 'https://github.com/HenryCaicedo/BungeeJumpingSimulator',
+},
+{
+    name: 'Women of Science',
+    description: "Educational game featuring Mary Jackson, Katherine Johnson, and Dorothy Vaughan as \"computers\" in early US space exploration.",
+    tags: ['Java', 'Game', 'Desktop'],
+    img: wos,
+    url: 'https://github.com/leovergaramarq/WomenOfScience',
+},
+{
+    name: 'COVID Simulator',
+    description: 'Graph-based COVID-19 spread simulator analyzing protective measures and contagion patterns.',
+    tags: ['Java', 'Simulator', 'Desktop'],
+    img: covidSimulator,
+    url: 'https://github.com/HenryCaicedo/Lab2_COVID_SIMULATOR',
+}
 ];
 
 export default projectsList;
